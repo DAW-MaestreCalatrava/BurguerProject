89877a5532286575bd074cfdc2bf1796170b7b04
acnagrellos <acnagrellos@hotmail.com>
2025-02-25 16:55:50 +0100

    feat: icons button (#8)
    Co-authored-by: aafd1904 <angelantoniofd19@gmail.com>

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import "./src/index.js";
+import "./src/footerComponents/instagram-icon.js";
+import "./src/footerComponents/globe-icon.js";
+import "./src/footerComponents/truck-icon.js";
+
 
 document.addEventListener("DOMContentLoaded", async () => {
 
