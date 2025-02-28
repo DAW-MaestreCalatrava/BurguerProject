c2912b97f2c1f82e9ee6fb15719dec1c854511c2
aafd1904 <angelantoniofd19@gmail.com>
2025-02-28 17:31:04 +0100

    footer

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import "./src/index.js";
+import "./src/footer/footer-component.js";
 import "./src/footerComponents/instagram-icon.js";
 import "./src/footerComponents/globe-icon.js";
 import "./src/footerComponents/truck-icon.js";
+
 
 
 document.addEventListener("DOMContentLoaded", async () => {
