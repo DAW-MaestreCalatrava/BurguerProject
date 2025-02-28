b664a58ef9e0444e0525d0b3b2b8175fcb7b2f5b
jaferse <157239043+jaferse@users.noreply.github.com>
2025-02-28 17:27:24 +0100

    feat: estructura de páginas

--- main.js
+++ main.js
@@ -7,16 +7,24 @@ document.addEventListener("DOMContentLoaded", async () => {
     const header = document.querySelector("nav-component");
 
     //Creamos una url
-    const url=new URL(location.href)
-    const position = url.pathname.substring(0,(url.pathname.length-1));
+    const url = new URL(location.href)
+    let position;
+    console.log(url.pathname.length);
+    
+    if (url.pathname.length == 1) {
+        position = url.pathname;
+    } else {
+        position = url.pathname.substring(0, (url.pathname.length - 1));
+
+    }
     console.log("Ubicación: ", position);
 
     //Lista de links
     const listItem = [
-        { texto: "Inicio", url: "/" , posicionActual: position},
-        { texto: "Contacta con nosotros", url: "/contacta" , posicionActual: position},
-        { texto: "Conócenos", url: "/conocenos" , posicionActual: position},
-        { texto: "componentes", url: "/components" , posicionActual: position},
+        { texto: "Inicio", url: "/", posicionActual: position },
+        { texto: "Contacta con nosotros", url: "/contacta", posicionActual: position },
+        { texto: "Conócenos", url: "/conocenos", posicionActual: position },
+        
     ];
 
 
