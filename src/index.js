47158bbafd0c76fb7446891fb1789533f357fe2f
Manuel Zamorano Cardell <manuelzamoranodaw@gmail.com>
2025-02-28 19:15:01 +0100

    feat: componente hero

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./components/nav-component/nav-component.js";
 import "./components/header-component/header-component.js";
 import "./components/card/card.js";
 import "./components/text/text.js";
+import "./components/hero-section/hero.js";
 import "./components/footerComponents/instagram-icon.js";
 import "./components/footerComponents/globe-icon.js";
 import "./components/footerComponents/truck-icon.js";
